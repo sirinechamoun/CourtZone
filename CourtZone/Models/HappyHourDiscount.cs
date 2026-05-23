a937995a736c531c9c567ba233481cf35fd32a0b
sirinechamoun <sirinechamoun7@gmail.com>
2026-05-23 21:01:50 +0300

    Update HappyHourDiscount.cs

--- CourtZone/Models/HappyHourDiscount.cs
+++ CourtZone/Models/HappyHourDiscount.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CourtZone.Models;
-
+// for discounts
 public class HappyHourDiscount
 {
     public int Id { get; set; }
